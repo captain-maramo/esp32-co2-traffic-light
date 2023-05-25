554895d7c483fa353c59d37096562fcbe846429d
captain-maramo <dsfreak93@googlemail.com>
2023-05-26 00:21:32 +0200

    first working version of CO2 traffic light

---
+++ esp32-co2-traffic-light.ino
@@ -0,0 +1,83 @@
+/****************************************************************************************
+ * 
+ * @file        ESP32_CO2_TrafficLight_With_LED.ino
+ * @author      captain-maramo
+ * @brief       Traffic light with ESP32, RGB LED and CCS811 Sensor with I2C comunication
+ *
+ *
+ * @copyright   [MIT License](http://opensource.org/licenses/MIT)
+ ****************************************************************************************/
+
+#include "CCS811-SOLDERED.h"
+#include "Wire.h"
+
+#define PIN_RED    16
+#define PIN_GREEN  17
+#define PIN_BLUE   18
+
+CCS_811 ccs811Sensor;
+
+void setup()
+{
+  setPinModes();
+  Wire.begin();
+  Serial.begin(115200);
+  ccs811Sensor.begin();
+}
+
+void loop()
+{
+  if (ccs811Sensor.dataAvailable())
+  {
+    int co2 = getCo2Value(ccs811Sensor);
+    printCo2ToSerial(co2);
+    setLedByCo2(co2);
+    delay(5000); // Don't spam the I2C bus
+  }
+}
+
+int getCo2Value(CCS_811 ccs811Sensor){
+  ccs811Sensor.readAlgorithmResults();
+  return ccs811Sensor.getCO2();
+}
+
+void printCo2ToSerial(int co2){
+  Serial.print("CO2["+String(co2)+"]");
+}
+
+void setLedByCo2(int co2){
+  if(co2<=999){
+    // Green
+    setLedColor(0, 255, 0);
+    printColorToSerial(0, 255, 0);
+  }
+  else if(co2>=1600){
+    // Red
+    setLedColor(255, 0, 0);
+    printColorToSerial(255, 0, 0);
+  }
+  else{
+    // Yellow
+    setLedColor(255, 155, 0);
+    printColorToSerial(255, 155, 0);
+  }
+}
+
+void setLedColor(int R, int G, int B) {
+  analogWrite(PIN_RED,   R);
+  analogWrite(PIN_GREEN, G);
+  analogWrite(PIN_BLUE,  B);
+}
+
+void printColorToSerial(int R, int G, int B){
+  String red = String(R);
+  String green = String(G);
+  String blue = String(B);
+  Serial.println("Red: " + red +" Green: " + green + " Blue: " + blue);
+}
+
+void setPinModes(){
+  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE,  OUTPUT);
+}
